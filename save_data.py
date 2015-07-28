7669cefab31d48eba1d61ac4c2f8c2e6c8b314f2
brnc69 <TristanHahn@gmx.net>
2015-07-28 17:21:25 +0200

    utf8 funzt

--- save_data.py
+++ save_data.py
@@ -2,9 +2,14 @@
 import json, codecs
 import pickle
 from pprint import pprint
+import sqlite3
+from itertools import izip
 
-with open('outfiles/playgrounds.json') as data_file:    
-    data = json.loads(data_file.read().decode("utf-8-sig"))
+conn = sqlite3.connect('spielplaetze.db')
+c = conn.cursor()
+jsonfile = codecs.open("outfiles/playgrounds.json", "r", "utf-8")
+data = json.loads(jsonfile.read())
+jsonfile.close()
 
 #Zeigt kurz die Keys in der json an
 """
@@ -35,7 +40,8 @@ Das heisst, dass die Spielplaetze über den Index von features aufgerufen werden
 output_file = codecs.open("outfiles/keys.txt", "w")
 
 for element in data["features"][0]["attributes"]:
-    pickle.dump(element, output_file) 
+    encoded_element = element.encode("utf-8")
+    pickle.dump(encoded_element, output_file) 
    
 output_file.close()
 
@@ -52,16 +58,56 @@ i = 0
 spielplatzname = []
 stadtteil = []
 stadtbezirk = []
+stadtviertel = []
+typ = []
+basketballkoerbe = []
+fussballtore = []
+skaterelemente = []
+tischtennisplatten = []
+torwand = []
+bemerkung = []
 
 while i < courts:
-     spielplatzname.append(data["features"][i]["attributes"]["Spielplatzname"])
+     sn = (data["features"][i]["attributes"]["Spielplatzname"])
+     spielplatzname.append(sn)
      
+     st = (data["features"][i]["attributes"]["Stadtteil"])
+     stadtteil.append(st)
+     
+     sb = (data["features"][i]["attributes"]["Stadtbezirk"])
+     stadtbezirk.append(sb)
+     
+     sv = (data["features"][i]["attributes"]["Stadtviertel"])
+     stadtviertel.append(sv)
+     
+     t = (data["features"][i]["attributes"]["Typ"])
+     typ.append(t)
+     
+     bk = (data["features"][i]["attributes"]["Basketballkoerbe"])
+     basketballkoerbe.append(bk)
+     
+     ft = (data["features"][i]["attributes"]["Fussballtore"])
+     fussballtore.append(ft)
+     
+     sk = (data["features"][i]["attributes"]["Skaterelemente"])
+     skaterelemente.append(sk)
+     
+     tp = (data["features"][i]["attributes"]["Tischtennisplatten"])
+     tischtennisplatten.append(tp)
+     
+     tw = (data["features"][i]["attributes"]["Torwand"])
+     torwand.append(tw)
+     
+     b = (data["features"][i]["attributes"]["Bemerkung"])
+     bemerkung.append(b)
+          
      i = i+1
+for x in stadtviertel:
+    print x
 
 
-bla = codecs.open("outfiles/bla.txt", "w",encoding="utf-8")
+"""    
 for item in spielplatzname:
-  bla.write("%s\n" % item)
-
-bla.close()
-
+  c.execute('insert into daten(Spielplatzname) values (:item)', {'item':item})
+db.commit()
+"""
